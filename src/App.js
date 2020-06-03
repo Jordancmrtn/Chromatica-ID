2234d969e2a76d418b0d97b7577b92bf2f9ccd07
Jordan Caumartin <pro.jordanc@gmail.com>
2020-06-03 13:56:53 +0200

    add section3 and footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css'
 import Header from './components/Header'
 import Title from './components/Title'
 import Section2 from './components/Section2'
+import Section3 from './components/Section3'
 import CtxUser from './context/CtxUser'
 import Form from './components/Form'
 
@@ -23,6 +24,7 @@ function App() {
         <Title/>
         <Form/>
         <Section2/>
+        <Section3/>
       </CtxUser.Provider>
     </div>
   );
---
+++ src/components/Section3/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ImageAlbum from '../../assets/ChromaticaAlbumInfos.png'
+import './section3.css'
+
+export default function Section3() {
+  return (
+    <>
+      <img src={ImageAlbum} id="imgInfosAlbum" alt="Infos album Chromatica form Lady Gaga"/>
+      <p id="infosFooter">CREATED BY <a href="https://www.linkedin.com/in/jordancaumartin/" target="_blank">JORDAN</a> - French LittleMonster - @2020</p>
+    </>
+  )
+}
