596213a9639a14c5a197321e12a57f55313a756e
Jordan Caumartin <pro.jordanc@gmail.com>
2020-06-03 12:28:02 +0200

    create context for the user

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './App.css'
 import Header from './components/Header'
 import Title from './components/Title'
 import Section2 from './components/Section2'
+import CtxUser from './Context/CtxUser'
 
 function App() {
+
+  const [user, setUser] = useState({
+    name : null,
+    age: null,
+    country: null,
+    tribe: null
+  });
+
+
   return (
     <div className="App">
-      <Header/>
-      <Title/>
-      <Section2/>
+      <CtxUser.Provider value={user}>
+        <Header/>
+        <Title/>
+        <Section2/>
+      </CtxUser.Provider>
     </div>
   );
 }
