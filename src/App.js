55daebb3baf24e479107ad34dad506c0502cec3a
Jordan Caumartin <pro.jordanc@gmail.com>
2020-06-05 16:25:20 +0200

    implementation of the router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, {useState} from 'react'
 import './App.css'
-import Header from './components/Header'
-import Title from './components/Title'
-import Section2 from './components/Section2'
-import Section3 from './components/Section3'
+import CardID from './components/page/cardID.js'
 import CtxUser from './context/CtxUser'
-import Form from './components/Form'
 import CtxToogle from './context/CtxToogle'
+import { Switch, Route } from 'react-router-dom';
+
 
 function App() {
 
@@ -24,11 +22,9 @@ function App() {
     <div className="App">
       <CtxToogle.Provider value={[toogle, setToogle]}>
       <CtxUser.Provider value={[user, setUser]}>
-        <Header/>
-        <Title/>
-        <Form/>
-        <Section2/>
-        <Section3/>
+        <Switch>
+          <Route exact path="/" component={CardID} />
+        </Switch>
       </CtxUser.Provider>
       </CtxToogle.Provider>
     </div>
---
+++ src/components/page/cardID.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from '../Header'
+import Title from '../Title'
+import Form from '../Form'
+import Section2 from '../Section2'
+import Section3 from '../Section3'
+
+export default function cardID() {
+  return (
+    <>
+      <Header/>
+      <Title/>
+      <Form/>
+      <Section2/>
+      <Section3/>
+    </>
+  )
+}
