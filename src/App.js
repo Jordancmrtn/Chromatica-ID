ee041ca80cf392852325808bfbf2e2904fe0cab5
Jordan Caumartin <pro.jordanc@gmail.com>
2020-06-06 14:04:17 +0200

    create a context for answers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import './App.css'
+import { Switch, Route } from 'react-router-dom'
 
 import CardID from './page/CardID'
 import Quizz from './page/Quizz/Quizz'
@@ -7,8 +7,10 @@ import QuizzQuestion from './page/QuizzQuestion/QuizzQuestion'
 
 import CtxUser from './context/CtxUser'
 import CtxToogle from './context/CtxToogle'
+import CtxAnswer from './context/CtxAnswer'
 
-import { Switch, Route } from 'react-router-dom';
+import './App.css'
+
 
 
 function App() {
@@ -27,13 +29,16 @@ function App() {
     <div className="App">
       <CtxToogle.Provider value={[toogle, setToogle]}>
       <CtxUser.Provider value={[user, setUser]}>
+      <CtxAnswer.Provider value={}>
         <Switch>
           <Route exact path="/" component={CardID} />
           <Route exact path="/quizz" component={Quizz} />
           <Route exact path="/quizz/:id" component={QuizzQuestion} />
         </Switch>
+      </CtxAnswer.Provider>
       </CtxUser.Provider>
       </CtxToogle.Provider>
+
     </div>
   );
 }
---
+++ src/context/CtxAnswer.js
@@ -0,0 +1,7 @@
+//import { createContext } from './node_modules/react';
+import React from 'react'
+
+//Create the context for share information everywhere with hooks
+const CtxAnswer = React.createContext([]);
+
+export default CtxAnswer;
